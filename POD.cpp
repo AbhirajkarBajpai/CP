6b2c9c274f0778b2abdf8551f757712ca1febb75
Abhirajkar Bajpai <122393161+AbhirajkarBajpai@users.noreply.github.com>
2024-03-10 22:55:55 +0530

    Update POD.cpp

--- POD.cpp
+++ POD.cpp
@@ -1,1 +1,15 @@
+// GFG Problem of the Day : 10 March , 2024
 
+string removeDuplicates(string str) {
+	    string ans;
+	    set<char>s;
+	    for(int i=0;i<str.size();i++){
+	        if(s.find(str[i])==s.end()){
+	            s.insert(str[i]);
+	            ans.push_back(str[i]);
+	        }
+	        
+	    }
+
+	    return ans;
+	}
